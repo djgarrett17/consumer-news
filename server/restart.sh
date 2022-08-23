dae1686e32b40a9d3c158577b1e6a3fc1c248ff3
Devin Garrett <poweruser@Ryans-iMac.local>
2022-08-23 11:20:08 -0500

    changes made to the project47

--- server/restart.sh
+++ server/restart.sh
@@ -1,4 +1,6 @@
 
 #!/usr/local/bin/heroku
 echo "restart here2"
-heroku restart
+curl -n -X DELETE https://api.heroku.com/apps/consumer-e-newsletter/dynos \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3"
