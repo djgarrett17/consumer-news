55e0428701e8a1c8b1a13025841cb4e577d8c7d2
Devin Garrett <poweruser@Ryans-iMac.local>
2022-08-23 13:54:37 -0500

    changes made to the project63

--- server/restart.sh
+++ server/restart.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 echo "restart here2"
 echo $HEROKU_API_KEY
-curl -n -X DELETE http://api.heroku.com/apps/consumer-e-newsletter/dynos \
+curl -n -X DELETE https://api.heroku.com/apps/consumer-e-newsletter/dynos \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3" \
   -H "Authorization: Bearer $HEROKU_API_KEY"
