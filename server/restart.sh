7bad830219625fa18023802e07f8ca6c0d12a7ef
Devin Garrett <poweruser@Ryans-iMac.local>
2022-08-23 11:37:01 -0500

    changes made to the project50

--- server/restart.sh
+++ server/restart.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
-. ./server/index.js
+. ./env
 echo "restart here2"
-echo $api
+echo $REACT_APP_API_KEY
 curl -n -X DELETE https://api.heroku.com/apps/consumer-e-newsletter/dynos \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3"
-  -H "Authorization: Bearer ${api}"
+  -H "Authorization: Bearer ${REACT_APP_API_KEY}"
