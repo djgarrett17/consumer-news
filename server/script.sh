97d034590857444b0b6781b74518fd49d8154662
Devin Garrett <poweruser@Ryans-iMac.local>
2022-08-31 11:54:31 -0500

    banners push

--- server/script.sh
+++ server/script.sh
@@ -8,10 +8,13 @@ echo $contentTitle
 echo $landingLink
 echo $imageHeader
 echo $image
+echo $phone
+echo $email
+echo $website
 echo $noContent
 echo $singlestring
 echo $semicolon
 echo $forwardSlash
 
 
-sed -e "s/\${content}/$content/" -e "s/\${contentTitle}/$contentTitle/" -e "s/\${landingLink}/$landingLink/" -e "s/\${imageHeader}/$imageHeader/" -e "s/\${image}/$image/" -e "s/\${none}/$noContent/g" -e "s/\${semicolon}/$semicolon/g" -e "s/\${single}/$singlestring/g" -e "s%\${forwardSlash}%$forwardSlash%g" ./server/consumer.txt > ./server/consumer-1.txt
+sed -e "s/\${content}/$content/" -e "s/\${contentTitle}/$contentTitle/" -e "s/\${landingLink}/$landingLink/" -e "s/\${imageHeader}/$imageHeader/" -e "s/\${image}/$image/" -e "s/\${phoneNumber}/$phone/" -e "s/\${emailAddress}/$email/g" -e "s/\${websiteUrl}/$website/g" -e "s/\${none}/$noContent/g" -e "s/\${semicolon}/$semicolon/g" -e "s/\${single}/$singlestring/g" -e "s%\${forwardSlash}%$forwardSlash%g" ./server/consumer-2.txt > ./server/consumer-1.txt
